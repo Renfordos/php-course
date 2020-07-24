5136e243ded250bb9183a141e9f1b9bb322ac397
Renfordos <68708177+Renfordos@users.noreply.github.com>
2020-07-24 15:01:14 +0300

    add variables

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP Store</title>
+</head>
+<body>
+<?php
+$name = "PHP store";
+$credit = 1000;
+$recomend = "Please save your money for the future";
+echo "<h1>Welcome to ".$name."!</h1>";
+echo "<h2>You have $".$credit." in your wallet.</h2>";
+echo "<h3>".$recomend."!</h3>";
+?>
+</body>
+</htm>
