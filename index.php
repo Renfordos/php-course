72b0fb69aa4a4ecb096345679dd22a787b91f337
Renfordos <68708177+Renfordos@users.noreply.github.com>
2020-07-24 16:07:31 +0300

    add conditional statement

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body>
 <?php
 $name = "PHP store";
-$credit = 1000;
+$credit = 150;
 $recomend = "Please save your money for the future";
 echo "<h1>Welcome to ".$name."!</h1>";
 echo "<h2>You have $".$credit." in your wallet.</h2>";
@@ -16,9 +16,15 @@ $products['CPU']=200;
 $products['RAM']=70;
 $products['Motherboard']=100;
 
-foreach($products as $key => $value){
+ foreach($products as $key => $value){
     echo "<p>The ".$key." costs ".$value."</p>";
-}
-?>
-</body>
-</htm>
+  }
+  echo "<h2>Items you can afford</h2>";
+   foreach($products as $key => $value){
+    if($value <= $credit ){
+    	echo "<p>".$key."</p>"; 
+		    }
+	    }
+    ?>
+  </body>
+</html>
